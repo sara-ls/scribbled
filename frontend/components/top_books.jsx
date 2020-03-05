1cd16799a5877f79ecee48f7e9398c98ccf10302
Sara Sampson <sarasampson.sls@gmail.com>
2020-03-04 17:42:11 -0800

    Fix all asset image sources

--- frontend/components/top_books.jsx
+++ frontend/components/top_books.jsx
@@ -6,31 +6,28 @@ class TopBooks extends React.Component {
       <div className="top-books">
         <h2>All of the Best Books</h2>
         <div className="book-covers-container">
-          <img className="book-cover" src="assets/book-covers/kill.jpg" />
+          <img className="book-cover" src={window.killURL} />
           <img className="book-cover" src={window.chemistryURL} />
           <img className="book-cover" src={window.lobsterURL} />
-          <img className="book-cover" src="assets/book-covers/DearGirls.jpg" />
-          <img className="book-cover" src="assets/book-covers/fleabag.jpg" />
-          <img className="book-cover" src="assets/book-covers/sapiens.jpg" />
-          <img className="book-cover" src="assets/book-covers/homo-deus.jpg" />
+          <img className="book-cover" src={window.girlsURL} />
+          <img className="book-cover" src={window.fleaURL} />
+          <img className="book-cover" src={window.sapiensURL} />
+          <img className="book-cover" src={window.deusURL} />
           <img
             className="book-cover"
-            src="assets/book-covers/trick-mirror.jpg"
+            src={window.trickURL}
           />
           <img className="book-cover" src={window.bad_blood_coverURL} />
           <img
             className="book-cover"
-            src="assets/book-covers/My_Year_of_Rest_and_Relaxation.jpg"
+            src={window.restURL}
           />
           <img className="book-cover" src={window.schizo_coverURL} />
           <img className="book-cover" src={window.animalsURL} />
-          <img className="book-cover" src="assets/book-covers/stories.jpg" />
-          <img className="book-cover" src="assets/book-covers/talk.jpg" />
-          <img className="book-cover" src="assets/book-covers/enlight.jpg" />
-          <img
-            className="book-cover"
-            src="assets/book-covers/factfulness.jpg"
-          />
+          <img className="book-cover" src={window.storiesURL} />
+          <img className="book-cover" src={window.talkURL} />
+          <img className="book-cover" src={window.enlightURL} />
+          <img className="book-cover" src={window.factURL} />
         </div>
       </div>
     );
