73cd3e0c84ace06baa5f2183485ea7c94385cc46
Sara Sampson <sarasampson.sls@gmail.com>
2020-03-13 11:46:23 -0700

    Fix fade in animation

--- frontend/components/files/books/books_index_item.jsx
+++ frontend/components/files/books/books_index_item.jsx
@@ -13,49 +13,51 @@ class BookIndexItem extends React.Component {
         ) : null} */}
         <div className="book-index-img-container">
           {/* <Link to={`/books/${this.props.book.id}`}> */}
-            <img src={this.props.book.cover_url} height="205" />
+          <img src={this.props.book.cover_url} height="205" />
           {/* </Link> */}
         </div>
-        <div className="index-title">
-          <Link to={`/books/${this.props.book.id}`}>
-            <div>{this.props.book.title}</div>
-          </Link>
-        </div>
-        <div className="index-author">{this.props.book.author}</div>
-        <div className="small-rating rating-wrapper" id="xs-star">
-          {this.props.book.average_rating ? (
-            <Rating
-              readonly
-              initialRating={this.props.book.average_rating}
-              emptySymbol={
-                <img
-                  src={window.starEmptyURL}
-                  opacity="0"
-                  height="15px"
-                  margin-right="5px"
-                  className="icon"
-                  id="empty-star"
-                />
-              }
-              fullSymbol={
-                <img
-                  src={window.redStarFullURL}
-                  height="15px"
-                  margin-right="5px"
-                  className="icon"
-                  id="color-star"
-                />
-              }
-            />
-          ) : (
-            <span></span>
-          )}
-        </div>
-        {this.props.description ? (
-          <div className="description-contain">
-            {this.props.book.description}
+        <div className="top-div">
+          <div className="index-title">
+            <Link to={`/books/${this.props.book.id}`}>
+              <div>{this.props.book.title}</div>
+            </Link>
           </div>
-        ) : null}
+          <div className="index-author">{this.props.book.author}</div>
+          <div className="small-rating rating-wrapper" id="xs-star">
+            {this.props.book.average_rating ? (
+              <Rating
+                readonly
+                initialRating={this.props.book.average_rating}
+                emptySymbol={
+                  <img
+                    src={window.starEmptyURL}
+                    opacity="0"
+                    height="15px"
+                    margin-right="5px"
+                    className="icon"
+                    id="empty-star"
+                  />
+                }
+                fullSymbol={
+                  <img
+                    src={window.redStarFullURL}
+                    height="15px"
+                    margin-right="5px"
+                    className="icon"
+                    id="color-star"
+                  />
+                }
+              />
+            ) : (
+              <span></span>
+            )}
+          </div>
+          {this.props.description ? (
+            <div className="description-contain">
+              {this.props.book.description}
+            </div>
+          ) : null}
+        </div>
       </div>
     );
   }
