814ee8fe15fbe9220af3583aef06d5bb717b3107
Sara Sampson <sarasampson.sls@gmail.com>
2020-04-09 13:07:03 -0700

    Improve accessibility

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -60,6 +60,7 @@ const Splash = ({ openModal }) => {
         <h2>All of the Best Books</h2>
         <div className="book-covers-container">
           <img
+            alt="book-cover"
             className="book-cover"
             src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1479350390l/31684925._SY475_.jpg"
           />
@@ -67,31 +68,53 @@ const Splash = ({ openModal }) => {
             className="book-cover"
             src="https://images-na.ssl-images-amazon.com/images/I/41LuDLgf0lL._SX331_BO1,204,203,200_.jpg"
           />
-          <img className="book-cover" src={window.girlsURL} />
-          <img className="book-cover" src={window.fleaURL} />
+          <img alt="book-cover" className="book-cover" src={window.girlsURL} />
+          <img alt="book-cover" className="book-cover" src={window.fleaURL} />
           <img
             className="book-cover"
             src="https://images-na.ssl-images-amazon.com/images/I/51Sn8PEXwcL.jpg"
           />
-          <img className="book-cover" src={window.deusURL} />
+          <img alt="book-cover" className="book-cover" src={window.deusURL} />
           <img
+            alt="book-cover"
             className="book-cover"
             src="https://images-na.ssl-images-amazon.com/images/I/41rOxRzBevL.jpg"
           />
           <img
+            alt="book-cover"
             className="book-cover"
             src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1544069605l/43126457.jpg"
           />
-          <img className="book-cover" src={window.bad_blood_coverURL} />
+          <img
+            alt="book-cover"
+            className="book-cover"
+            src={window.bad_blood_coverURL}
+          />
           <img
             className="book-cover"
             src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1544069605l/43126457.jpg"
           />
-          <img className="book-cover" src={window.schizo_coverURL} />
-          <img className="book-cover" src={window.animalsURL} />
-          <img className="book-cover" src={window.storiesURL} />
-          <img className="book-cover" src={window.talkURL} />
-          <img className="book-cover" src={window.enlightURL} />
+          <img
+            alt="book-cover"
+            className="book-cover"
+            src={window.schizo_coverURL}
+          />
+          <img
+            alt="book-cover"
+            className="book-cover"
+            src={window.animalsURL}
+          />
+          <img
+            alt="book-cover"
+            className="book-cover"
+            src={window.storiesURL}
+          />
+          <img alt="book-cover" className="book-cover" src={window.talkURL} />
+          <img
+            alt="book-cover"
+            className="book-cover"
+            src={window.enlightURL}
+          />
           <img
             className="book-cover"
             src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1544964112l/40933227._SY475_.jpg"
