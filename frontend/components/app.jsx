d7fb73fb206feae05e8238ac03776af08882945e
Sara Sampson <sarasampson.sls@gmail.com>
2020-04-16 11:55:04 -0700

    Refactor ScrollToTop

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -13,6 +13,7 @@ import Saved from "./files/saved/saved";
 import UploadForm from "./files/documents/upload";
 import Account from "./user_session/account";
 import Book from "./files/books/book_show";
+import ScrollToTop from "./ui/scroll_top";
 
 class App extends React.Component {
   render() {
@@ -20,25 +21,32 @@ class App extends React.Component {
       <div className="app">
         <NavBar />
         <Modal id="form" />
-        <div className="main">
-          {/* Render Home page as main content component if logged in */}
-          <ProtectedRoute exact path="/" component={Home} />{" "}
-          <Switch>
-            {/* Render Splash page as main content component if logged out */}
-            <AuthRoute exact path="/splash" component={Splash} />
+        <ScrollToTop>
+          <div className="main">
+            <Switch>
+              {/* Render Home page as main content component if logged in */}
+              <ProtectedRoute exact path="/" component={Home} />
+              {/* Render Splash page as main content component if logged out */}
+              <AuthRoute exact path="/splash" component={Splash} />
 
-            <ProtectedRoute exact path="/books" component={BookIndex} />
-            <ProtectedRoute exact path="/books/:id" component={Book} />
-            <ProtectedRoute exact path="/documents" component={DocumentIndex} />
-            <ProtectedRoute exact path="/topcharts" component={TopCharts} />
-            <ProtectedRoute exact path="/saved" component={Saved} />
-            <ProtectedRoute exact path="/upload" component={UploadForm} />
-            <ProtectedRoute exact path="/account" component={Account} />
-            {/* Default redirect given bad path */}
-            <Redirect path="*" to="/books" />
-          </Switch>
-          <Footer />
-        </div>
+              <ProtectedRoute exact path="/books" component={BookIndex} />
+              <ProtectedRoute exact path="/books/:id" component={Book} />
+              <ProtectedRoute
+                exact
+                path="/documents"
+                component={DocumentIndex}
+              />
+              <ProtectedRoute exact path="/topcharts" component={TopCharts} />
+              <ProtectedRoute exact path="/saved" component={Saved} />
+              <ProtectedRoute exact path="/upload" component={UploadForm} />
+              <ProtectedRoute exact path="/account" component={Account} />
+              {/* Default redirect given bad path */}
+              <Redirect path="*" to="/books" />
+            </Switch>
+
+            <Footer />
+          </div>
+        </ScrollToTop>
       </div>
     );
   }
--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { Route, Redirect, Switch, Link, HashRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./app";
-import ScrollToTop from "./scroll_top";
 
 const Root = ({ store }) => {
   return (
     <Provider store={store}>
       <HashRouter>
-        <ScrollToTop>
-          <App />
-        </ScrollToTop>
+        <App />
       </HashRouter>
     </Provider>
   );
