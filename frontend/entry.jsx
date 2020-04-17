5c33c610aa1a467022879bf945718add991ed860
Sara Sampson <sarasampson.sls@gmail.com>
2020-04-17 15:58:41 -0700

    Remove logger

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Root from './components/root';
-import configureStore from './store/store';
+import React from "react";
+import ReactDOM from "react-dom";
+import Root from "./components/root";
+import configureStore from "./store/store";
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   let store = configureStore();
 
   if (window.currentUser) {
     const preloadedState = {
       session: { id: window.currentUser.id },
       entities: {
-        users: { [window.currentUser.id]: window.currentUser }
+        users: { [window.currentUser.id]: window.currentUser },
       },
       ui: {
-        modal: null
-      }
+        modal: null,
+      },
     };
     store = configureStore(preloadedState);
     delete window.currentUser;
-  } 
+  }
 
-  const root = document.getElementById('root');
+  const root = document.getElementById("root");
   ReactDOM.render(<Root store={store} />, root);
 });
